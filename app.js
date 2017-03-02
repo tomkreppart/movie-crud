0b2a617a6e6dced8c3babc956b284775d87a9053
Tom Krepp <tommykrepp@aol.com>
2017-03-01 17:13:26 -0700

    still deployingggggg maybe

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const routes = require('./routes.js');
 const bodyParser = require('body-parser');
 
+var PORT = process.env.PORT || 5000;
+
 app.use(express.static('./public', {index:"home.html"}))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json());
 app.use('/', routes);
 
-app.listen(5000, function () {
-    console.log('Server listening on port 5000');
+app.listen(PORT, function () {
+    console.log('Server listening on port: ' + PORT);
 });
